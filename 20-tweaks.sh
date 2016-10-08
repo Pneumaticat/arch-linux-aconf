334259c122b8ad4703c1912df9a311d3b5bc99ea
Kevin Liu <kevin@potatofrom.space>
2016-10-08 11:17:37 -0400

    Re-enable OOM-killer

--- 20-tweaks.sh
+++ 20-tweaks.sh
@@ -1,4 +1,3 @@
 CopyFile /etc/systemd/system/lvm2-pvscan@.service.d/override.conf
-CopyFile /etc/sysctl.d/99_overcommit.conf
 CopyFile /etc/systemd/journald.conf.d/fw-tty12.conf 
 CopyFile /etc/systemd/system.conf
