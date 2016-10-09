72d19dae6acdc71d57a60b1831a37b2e206c1cba
Kevin Liu <kevin@potatofrom.space>
2016-10-09 16:11:48 -0400

    Remove default linux and linux-headers

--- 20-kernels.sh
+++ 20-kernels.sh
@@ -1,4 +1,2 @@
-AddPackage linux # The Linux kernel and modules
-AddPackage linux-headers # Header files and scripts for building modules for Linux kernel
 AddPackage --foreign linux-ck # Linux Kernel with the ck5 patchset featuring the Brain Fuck Scheduler v0.502.
 AddPackage --foreign linux-ck-headers # Header files and scripts to build modules for linux-ck.
