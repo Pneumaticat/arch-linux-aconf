ba146840747008a00174d5e6080fe2773a5bd238
Kevin Liu <kevin@potatofrom.space>
2016-10-09 16:11:13 -0400

    Enable NVIDIA KMS (and bfq io scheduler)

--- 20-nvidia.sh
+++ 20-nvidia.sh
@@ -5,3 +5,5 @@ AddPackage nvidia-libgl # NVIDIA drivers libraries symlinks
 AddPackage nvidia-settings # Tool for configuring the NVIDIA graphics driver
 AddPackage opencl-nvidia # OpenCL implemention for NVIDIA
 AddPackage lib32-nvidia-libgl # NVIDIA drivers libraries symlinks (32-bit) 
+
+CopyFile /etc/pacman.d/hooks/nvidia.hook # Rebuild initramfs on nvidia-dkms upgrade
