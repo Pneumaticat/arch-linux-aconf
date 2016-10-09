0a8f61c471fdb390f11655b2dbf21e8219fc7d5e
Kevin Liu <kevin@potatofrom.space>
2016-10-09 16:13:22 -0400

    Remove snapper

--- 20-snapper.sh
+++
@@ -1,8 +0,0 @@
-AddPackage snapper # A tool for managing BTRFS and LVM snapshots. It can create, diff and restore snapshots and provides timelined auto-snapping.
-AddPackage --foreign snap-pac # Pacman hooks that use snapper to create pre/post btrfs snapshots like openSUSE's YaST
-AddPackage --foreign snapper-gui-git # Gui for snapper, a tool of managing snapshots of Btrfs subvolumes and LVM volumes
-CopyFile /etc/snapper/configs/root 640
-CopyFile /etc/conf.d/snapper
-
-CreateLink /etc/systemd/system/basic.target.wants/snapper-cleanup.timer /usr/lib/systemd/system/snapper-cleanup.timer
-CreateLink /etc/systemd/system/basic.target.wants/snapper-timeline.timer /usr/lib/systemd/system/snapper-timeline.timer
