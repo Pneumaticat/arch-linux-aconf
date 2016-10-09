ff2599853e78d64ad25f780edc287635dd3c37d3
Kevin Liu <kevin@potatofrom.space>
2016-10-09 16:14:00 -0400

    Change auto-suspend management to use shell script

--- 20-suspend-management.sh
+++ 20-suspend-management.sh
@@ -1,12 +1,10 @@
-CopyFile /etc/systemd/system/suspend.target.d/override.conf
+# Suspend to hibernate
+CopyFile /etc/systemd/system/suspend.target.d/override.conf # Edited to make suspend-to-hibernate work
 CopyFile /etc/systemd/system/suspend-to-hibernate.service
+CreateLink /etc/systemd/system/sleep.target.wants/suspend-to-hibernate.service /etc/systemd/system/suspend-to-hibernate.service
+
+# Auto-hibernate and resume on a schedule
 CopyFile /etc/systemd/system/auto-hibernate.service
 CopyFile /etc/systemd/system/auto-hibernate.timer
-CopyFile /etc/systemd/system/auto-resume-m-f.timer
-CopyFile /etc/systemd/system/auto-resume.service
-CopyFile /etc/systemd/system/auto-resume-s-s.timer
-
-CreateLink /etc/systemd/system/sleep.target.wants/suspend-to-hibernate.service /etc/systemd/system/suspend-to-hibernate.service
-CreateLink /etc/systemd/system/multi-user.target.wants/auto-resume-m-f.timer /etc/systemd/system/auto-resume-m-f.timer
-CreateLink /etc/systemd/system/multi-user.target.wants/auto-resume-s-s.timer /etc/systemd/system/auto-resume-s-s.timer
+CopyFile /usr/local/sbin/auto-hibernate-and-resume 755
 CreateLink /etc/systemd/system/timers.target.wants/auto-hibernate.timer /etc/systemd/system/auto-hibernate.timer
