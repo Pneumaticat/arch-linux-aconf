0d5f4c90e6763eb7fdcb0cb57fbc9870cc51a6cf
Kevin Liu <kevin@potatofrom.space>
2016-10-09 16:12:21 -0400

    Add grub hook to remake grub.cfg

--- 20-grub.sh
+++ 20-grub.sh
@@ -1,7 +1,8 @@
-AddPackage grub # GNU GRand Unified Bootloader (2) 
+AddPackage grub # GNU GRand Unified Bootloader (2)
+AddPackage --foreign grub-hook
 AddPackage os-prober # Utility to detect other OSes on a set of drives
-CopyFile /etc/default/grub
 
+CopyFile /etc/default/grub
 CopyFile /etc/grub.d/01_grub-customizer_menu_color_helper 755
 CopyFile /etc/grub.d/10_linux 755
 CopyFile /etc/grub.d/.script_sources.txt
