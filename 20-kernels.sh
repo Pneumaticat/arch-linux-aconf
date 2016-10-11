d636b666a7ade99bc886c7e379665b80a9db3712
Kevin Liu <kevin@potatofrom.space>
2016-10-11 13:02:34 -0400

    Add linux-lts-ck and linux-lts-ck-headers

--- 20-kernels.sh
+++ 20-kernels.sh
@@ -1,2 +1,4 @@
+AddPackage --foreign linux-lts-ck # Linux Kernel with the ck1 patchset featuring the Brain Fuck Scheduler v0.467.
+AddPackage --foreign linux-lts-ck-headers # Header files and scripts to build modules for linux-lts-ck.
 AddPackage --foreign linux-ck # Linux Kernel with the ck5 patchset featuring the Brain Fuck Scheduler v0.502.
 AddPackage --foreign linux-ck-headers # Header files and scripts to build modules for linux-ck.
