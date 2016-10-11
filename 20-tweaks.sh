90d04f8ccb934a7d5a6e897308705b95266be392
Kevin Liu <kevin@potatofrom.space>
2016-10-11 13:09:28 -0400

    Add overcommit sysctl tweak

--- 20-tweaks.sh
+++ 20-tweaks.sh
@@ -1,3 +1,4 @@
 CopyFile /etc/systemd/system/lvm2-pvscan@.service.d/override.conf
 CopyFile /etc/systemd/journald.conf.d/fw-tty12.conf 
 CopyFile /etc/systemd/system.conf
+CopyFile /etc/sysctl.d/99-overcommit.conf
