3490c7b17cc25ca71688c914dc22c05d601cbf87
Kevin Liu <kevin@potatofrom.space>
2016-10-11 13:05:39 -0400

    Rename shutdown & resume services, timers

--- 20-suspend-management.sh
+++ 20-suspend-management.sh
@@ -4,7 +4,7 @@ CopyFile /etc/systemd/system/suspend-to-hibernate.service
 CreateLink /etc/systemd/system/sleep.target.wants/suspend-to-hibernate.service /etc/systemd/system/suspend-to-hibernate.service
 
 # Auto-hibernate and resume on a schedule
-CopyFile /etc/systemd/system/auto-hibernate.service
-CopyFile /etc/systemd/system/auto-hibernate.timer
-CopyFile /usr/local/sbin/auto-hibernate-and-resume 755
-CreateLink /etc/systemd/system/timers.target.wants/auto-hibernate.timer /etc/systemd/system/auto-hibernate.timer
+CopyFile /etc/systemd/system/auto-shutdown.service
+CopyFile /etc/systemd/system/auto-shutdown.timer
+CreateLink /etc/systemd/system/timers.target.wants/auto-shutdown.timer /etc/systemd/system/auto-shutdown.timer
+CopyFile /usr/local/sbin/auto-shutdown-and-resume 755
