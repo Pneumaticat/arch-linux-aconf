a8ce80b7bc97dd9d0d008c6fdff30a9a5adbdd70
Kevin Liu <kevin@potatofrom.space>
2016-10-11 13:10:47 -0400

    Comment 20-tweaks.sh

--- 20-tweaks.sh
+++ 20-tweaks.sh
@@ -1,4 +1,8 @@
+# Allow partial LVM scans (to prevent failed lvm scan unit on startup)
 CopyFile /etc/systemd/system/lvm2-pvscan@.service.d/override.conf
-CopyFile /etc/systemd/journald.conf.d/fw-tty12.conf 
+# Forward journal output to TTY12
+CopyFile /etc/systemd/journald.conf.d/fw-tty12.conf
+# Enable colors during bootup; reduce service stop timeout to 30s
 CopyFile /etc/systemd/system.conf
+# Adjust overcommit ratio to 100%
 CopyFile /etc/sysctl.d/99-overcommit.conf
